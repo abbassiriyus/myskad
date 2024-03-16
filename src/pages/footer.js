b2fc9280df38f485f700a4172527188def7c22bd
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-03-16 15:39:03 +0500

    eee
    221ee

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import s from "../styles/footer.module.css"
-import { FaTelegramPlane,FaFacebookF } from "react-icons/fa";
-import { LiaYoutube } from "react-icons/lia";
-import { FaInstagram } from "react-icons/fa6";
 import img from "../img/image.png"
 import img1 from "../img/click.png"
+import img2 from "../img/telegram.png"
+import img3 from "../img/instagram.png"
+import img4 from "../img/insta.png"
+import img5 from "../img/facebook.png"
 import Image from 'next/image';
 export default function footer() {
   return (
@@ -18,20 +19,12 @@ export default function footer() {
             <p>info@rce.uz</p>
         </div>
         <div className={s.app}>
-        <h5>Мы в социальных сетяхМы в социальных сетях</h5>
+        <h5>Мы в социальных сетях</h5>
         <div className={s.app_1}>
-            <div className={s.telegram}>
-            <FaTelegramPlane />
-            </div>
-            <div className={s.youtube}>
-            <LiaYoutube />
-            </div>
-            <div className={s.instagram}>
-            <FaInstagram />
-            </div>
-            <div className={s.facebook}>
-            <FaFacebookF />
-            </div>
+           <Image src={img2} alt="" />
+           <Image src={img3} alt="" />
+           <Image src={img4} alt="" />
+           <Image src={img5} alt="" />
         </div>
             </div>
             <div className={s.pay}>
