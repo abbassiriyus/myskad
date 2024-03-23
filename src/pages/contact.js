f7431777e83245e9f15f4af3cc813c4b7caf9cfc
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-03-23 19:12:43 +0500

    223
    2342

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,8 +11,8 @@ export default function contact() {
 <Navbar/>
 <div className={s.page_road}>   
             <div className={s.span_p}>
-                <h1>О компании</h1>
-            <p><span >Главная</span> /О компании</p>
+                <h1>Контакт</h1>
+            <p><span >Главная</span> /Контакт</p>
             </div>
         </div>
         <div className={s.contact_sayt}>
@@ -68,7 +68,9 @@ export default function contact() {
             </div>
          
             <iframe className={s.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11990.515403730344!2d69.3711774!3d41.295181449999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef7caa606093d%3A0x4170e08da9eb76e7!2sAbror%20Ferma!5e0!3m2!1sru!2s!4v1711185301566!5m2!1sru!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>   </div>
+        </div>  
+         </div>
+         <Footer/>
     </div>
   )
 }
